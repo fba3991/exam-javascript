e654c1a3d65959612804e17cee8658eda4d142a4
Fabioes <fabionike@icloud.com>
2023-11-22 12:33:18 +0100

    feat / 3 esercizio completato

---
+++ esercizio3.js
@@ -0,0 +1,25 @@
+
+
+ const book = {
+    title: "Il Grande Gatsby",
+    author: "F. Scott Fitzgerald",
+    year: 1925
+};
+
+book.rating = 6;
+
+
+book.increaseRating = function(valoreIncremento) {
+    this.rating += valoreIncremento;
+}
+book.increaseRating(Math.floor(Math.random()* (4 -0 +1) +0));
+
+delete book.year;
+
+const keys = Object.keys(book);
+for (let i = 0; i < keys.length; i++) {
+    const key = keys[i]
+    console.log((`${key}: ${book[key]}`));
+}
+
+
